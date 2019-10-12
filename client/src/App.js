fcd6bb27d35504c0dacd244490c3f7f7d4593dfa
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-10-12 10:38:47 -0600

    polished file structer and changed App component to a class component and used axios to get player info

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios";
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      playerInfo: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/players")
+      .then(res => {
+        const playerAPI = res.data;
+        console.log(res.data)
+        this.setState({
+          playerInfo: playerAPI
+        })
+      })
+      .catch(err => {
+        console.log("ERROR", err)
+      })
+  }
+  
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    );
+
+
+  }
+ 
 }
 
 export default App;
